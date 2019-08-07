2c2ed8ec367b4f7ad5c59e4bd79d81879c2743fd
manorbar <53777215+manorbar@users.noreply.github.com>
2019-08-07 13:16:38 +0300

    Update nodejs.groovy

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -42,4 +42,29 @@ job('NodeJS Docker example') {
             skipDecorate()
         }
     }
+job('boilerplate-pipeline') {
+    scm {
+        git('git://github.com/yanivomc/docker-cicd') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('Node11101') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            buildContext('./misc/Jenkinsfile')
+            repositoryName('manorbar/docker-cicd') //qa / dev
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('manorbar')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
 }
