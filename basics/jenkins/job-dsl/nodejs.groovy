c1922c33080c4d6f197af87cac39475e866eecbd
manorbar <53777215+manorbar@users.noreply.github.com>
2019-08-07 13:58:56 +0300

    Update nodejs.groovy

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -66,25 +66,4 @@ pipelineJob('pipeline-boilerplate'){
     }
 }
 }
-pipelineJob('pipeline-boilerplate'){
-    def repo = 'https://github.com/jenkinsci/job-dsl-plugin.git'
-    
-        triggers {
-        scm('H/5 * * * *')
-    }
-    description("pipeline example")
-        definition {
-        cpsScm {
-            scm {
-                git{
-                    remote {url(repo)}
-                    branches('master', '**/feature*')
-                    scriptPath('./basics/misc/jenkinsfile')
-                    extensions{}
-                    
-            }
-        }
-    }
-}
-}
 
